ffe892abd52891d77f34d1751835432ef4b0e3dd
DiscordOdysseyDev <discordodysseybot@gmail.com>
2020-10-28 16:36:28 -0600

    First command

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const token = config.BOT_TOKEN;
+const prefix = config.PREFIX;
 
 const client = new Discord.Client();
 
-client.login(config.BOT_TOKEN);
+client.once('ready', ()=>{
+    console.log('Bot deployed succesfuly')
+})
 
-//test 2
+client.on('message', message=>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+})
+
+client.login(token);
