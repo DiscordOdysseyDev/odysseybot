78eca1c96c704a531cd4cc071566990893ac4d25
DiscordOdysseyDev <discordodysseybot@gmail.com>
2020-10-29 12:44:47 -0600

    createcountry now gathers data correctly

--- commands/createcountry.js
+++ commands/createcountry.js
@@ -1,7 +1,12 @@
 module.exports = {
     name: 'createcountry',
-    description: 'creates a country',
+    description: 'creates a country with the message author as the owner',
     execute(message, args){
-        message.channel.send('Hi!');
+        var name = args[0];
+        if(!args.length) {
+            message.channel.send('Syntax error, try again');
+            return;
+        }
+        message.channel.send('The country: ´' + name + '´will be created with ´' + message.author.username + '´ as the owner.');
     },
 };
